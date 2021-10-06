ea1e36cbfdf85f030387cbc41ca9c3e41b8b4f0c
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2021-10-06 12:52:28 +0530

    chalk + validate module

--- npmMod/index.js
+++ npmMod/index.js
@@ -1,3 +1,7 @@
 const chalk = require('chalk');
 
 console.log(chalk.green.bold.inverse("Success"));
+
+const v = require('validator');
+const res = v.isEmail("xyzs.com");
+console.log(res ? chalk.green.inverse(res) : chalk.red.inverse(res));

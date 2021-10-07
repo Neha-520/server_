2138d5888431f487b9f848ada2ddf3e060466a91
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2021-10-07 12:34:26 +0530

    wrapper module

---
+++ ModWrapperFunc/index.js
@@ -0,0 +1,9 @@
+
+(function (exports, require, module, __filename, __dirname) {
+    const n = "neha";
+    console.log(n);
+})();
+//module wrapper function in every module behind the scene and the parameters scope is private
+//like IIFE in js imediately invoked function expression
+console.log(__filename);
+console.log(__dirname);

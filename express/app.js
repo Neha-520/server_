c815dd8d728a3358e1ef6ac5764b2f73c39438ad
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2021-10-08 12:38:29 +0530

    sending response in json format

--- express/app.js
+++ express/app.js
@@ -3,12 +3,41 @@ const app = express();
 
 //parameters are route and callback funcn
 app.get("/", (req, res) => {
-    res.send("Hello world from express");
+    // res.send("Hello world from express");
+    res.write("Hello world from express");
+    res.write("Hello world from express");
+    res.send(); //close connection
 });
 
 app.get("/about", (req, res) => {
-    res.send("Hello world from about");
+    res.status(200).send("Hello world from about"); //by default express takes 200 status
 });
+
+app.get("/detail", (req, res) => {
+    res.send([{
+        id: 1,
+        name: "neha",
+    },
+    {
+        id: 2,
+        name: "dca",
+    },
+    ]); //express  converts it to json format by default
+});
+
+//2nd way
+app.get("/details", (req, res) => {
+    res.json([{
+        id: 3,
+        name: "neha",
+    },
+    {
+        id: 4,
+        name: "dca",
+    },
+    ]);
+}); //res.json will convert non-objects 
+//such as null which are not balid json
 
 app.listen(8000, () => {
     console.log("listening port at 8000")

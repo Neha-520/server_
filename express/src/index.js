2d8bf56b05efc1a0e80424e53ed2068b71e539de
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2021-10-08 15:27:03 +0530

    template engine hbs

--- express/src/index.js
+++ express/src/index.js
@@ -4,10 +4,20 @@ const app = express();
 const port = 8000;
 
 //use absolute path here
-app.use(express.static(path.join(__dirname, '../public')))
+const templatePath = path.join(__dirname, "../templates")
+
+app.set("view engine", "hbs");
+app.set('views', templatePath);
+
+// app.use(express.static(path.join(__dirname, '../public')))
+
 
 app.get("/", (req, res) => {
-    res.send("hello")
+    res.render("index")
+})
+
+app.get("/about", (req, res) => {
+    res.render("about")
 })
 
 app.listen(port, () => {

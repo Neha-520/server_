70361a9ab7ff4c37c62c0c130eb96d3bffb32108
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2021-10-08 15:46:54 +0530

    partials in express

--- express/src/index.js
+++ express/src/index.js
@@ -1,13 +1,15 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 const app = express();
 const port = 8000;
 
 //use absolute path here
-const templatePath = path.join(__dirname, "../templates")
+const templatePath = path.join(__dirname, "../templates/views")
 
 app.set("view engine", "hbs");
 app.set('views', templatePath);
+hbs.registerPartials(path.join(__dirname, '../templates/partials'))
 
 // app.use(express.static(path.join(__dirname, '../public')))
 
